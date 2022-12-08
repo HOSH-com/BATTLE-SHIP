530b6b7479cbc1d1122252eb5850ea9cf7aeb957
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2022-12-08 12:11:02 +0330

    update table

--- phase 1/main/main.c
+++ phase 1/main/main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include "welcome.h"
+#include "printTable.h"
 
 int player1[15][15] = {};   //maximum battle area table is 15*15
-int player2[15][15] = {};   //it starts from [1][1]
+int player2[15][15] = {};   
 
 int main()
 {
 
+    char pName1, pName2;
+    pName1 = 'a';
+    pName2 = 'b';
     
+    printTable(pName1 , pName2, player1, player2, 5);
     
     
     return 0;
--- phase 1/main/printTable.h
+++ phase 1/main/printTable.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-#define NO_SHIP 0;
-#define DESTROYED -1;
+#define NO_SHIP 0
+#define DESTROYED -1
 
 void printTable(char pName1, char pName2, int player1[][15], int player2[][15], int n)
 {
@@ -18,26 +18,52 @@ void printTable(char pName1, char pName2, int player1[][15], int player2[][15], 
     //printing:
 
         //first line:
-        printf("  ");                        //empty space 1
-        for (int i = 1; i <= n; i++) {      //col nom 1
-            printf ("%i ", i);
-        }
-         printf ("      |\t");               //empty space 2
-        for (int i = 1; i <= n; i++) {      //col nom 2
-            printf ("%i", i);
-        }
+            printf("  ");                        //section 1: empty space 1
+
+            for (int i = 1; i <= n; i++) {      //section 2: col nom 1
+                printf ("%i ", i);
+            }
+            printf ("\b\t|\t  ");                  //section 3: empty space 2
+
+            for (int i = 1; i <= n; i++) {      //section 4: col nom 2
+                printf ("%i ", i);
+            }
+            printf("\n");                        //next line
         
         //other lines:
-        for (int i = 0; i < n; i++)     //main loop
-        {
-            //section 1:
-            printf("%i", i+1);             //row nom 1
-            for ()
+            for (int i = 0; i < n; i++) {    //main loop
+        
+                printf("%i ", i+1);               //section 1: row nom 1
 
-            //section 2:
+                for (int j = 0; j < n; j++){    //section 2: player 1
 
-            //section 3:
+                    switch (player1[i][j]) {
+                        case NO_SHIP:
+                            printf("~ "); 
+                            break;
+                        case DESTROYED:
+                            printf("X "); 
+                            break;
+                        /*default:
+                            break;*/
+                    }
+                }
+                printf("\b\t|\t");               //section 3: empty space
 
-        }
+                for (int j = 0; j < n; j++){    //section 4: player 2
+                   
+                    switch (player2[i][j]) {
+                        case NO_SHIP:
+                            printf("~ "); 
+                            break;
+                        case DESTROYED:
+                            printf("X "); 
+                            break;
+                        /*default:
+                            break;*/
+                    }
+                }
+                printf("\n");       //next line
+            }
     
 }
