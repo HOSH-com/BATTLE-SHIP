9dea94a562746e0138e3aa3e6380a986bc29ba4f
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2022-12-08 11:13:59 +0330

    update

--- phase 1/main/printTable.h
+++ phase 1/main/printTable.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define NO_SHIP 0;
+#define DESTROYED -1;
+
 void printTable(char pName1, char pName2, int player1[][15], int player2[][15], int n)
 {
     /*  HESAM   code:    */
@@ -13,23 +16,28 @@ void printTable(char pName1, char pName2, int player1[][15], int player2[][15], 
 
 
     //printing:
-    printf ("  ");                  //empty space before 'player1'
-    for (int i = 1; i <= n; i++)   //player1
-    {
-        printf ("%i ", player1[0][i]);
-    }
-    
 
-    for (int i = 0; i < n; i++)     //main loop
-    {
+        //first line:
+        printf("  ");                        //empty space 1
+        for (int i = 1; i <= n; i++) {      //col nom 1
+            printf ("%i ", i);
+        }
+         printf ("      |\t");               //empty space 2
+        for (int i = 1; i <= n; i++) {      //col nom 2
+            printf ("%i", i);
+        }
+        
+        //other lines:
+        for (int i = 0; i < n; i++)     //main loop
+        {
+            //section 1:
+            printf("%i", i+1);             //row nom 1
+            for ()
 
-        //section 1:
-        for (int j1 =)
+            //section 2:
 
-        //section 2:
+            //section 3:
 
-        //section 3:
-
-    }
+        }
     
 }
