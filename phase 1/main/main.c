41668e06c414a50e75700530633a043dbf379c64
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2022-12-08 10:36:09 +0330

    new form

--- phase 1/main/main.c
+++ phase 1/main/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include "welcome.h"
 
+int player1[16][16] = {};   //maximum battle area table is 15*15
+int player2[16][16] = {};   //it starts from [1][1]
+
 int main()
 {
 
-    printf("_____\015hello");
-
+    
+    
+    
     return 0;
+    
 }
---
+++ phase 1/main/printTable.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void printTable(char pName1, char pName2, int player1[][15], int player2[][15], int n)
+{
+    /*  HESAM   code:    */
+
+
+
+
+
+
+    /*  REZA code:  */
+
+
+    //printing:
+    printf ("  ");                  //empty space before 'player1'
+    for (int i = 1; i <= n; i++)   //player1
+    {
+        printf ("%i ", player1[0][i]);
+    }
+    
+
+    for (int i = 0; i < n; i++)     //main loop
+    {
+
+        //section 1:
+        for (int j1 =)
+
+        //section 2:
+
+        //section 3:
+
+    }
+    
+}
