f490b867cebc3c2e6909f2e113232b1b1182f9c9
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2022-12-08 13:24:27 +0330

    mmd

--- phase 1/main/printTable.h
+++ phase 1/main/printTable.h
@@ -31,7 +31,7 @@ void printTable(char pName1[], char pName2[], int player1[][15], int player2[][1
     {
          printf(" ");
     }
-    printf("%d\n",pName2);
+    printf("%s\n",pName2);
     for (int i = 0; i <4*n+2 ; i++)
     {
          printf("-");
