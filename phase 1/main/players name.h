e2ac227f0932e834aa1f1394a5371b54493bcfdc
HESAM <hasam13831@gmail.com>
2022-12-08 13:09:23 +0330

    u

--- phase 1/main/players name.h
+++ phase 1/main/players name.h
@@ -23,7 +23,7 @@ void pNames (char pName1[], char pName2[],int n )
     {
          printf(" ");
     }
-    printf("%d\n",pName2);
+    printf("%s\n",pName2);
     for (int i = 0; i <4*n+2 ; i++)
     {
          printf("-");
