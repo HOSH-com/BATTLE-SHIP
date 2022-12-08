27de28a6cd989f6bc3c7aedbc3f37cccf3c01748
HESAM <hasam13831@gmail.com>
2022-12-08 12:49:01 +0330

    update

--- phase 1/main/players name.h
+++ phase 1/main/players name.h
@@ -9,19 +9,24 @@ int tedad_char(char p1[])
         }
     }
 }
-void pNames (char p1[], char p2[],int n )
+void pNames (char pName1[], char pName2[],int n )
 {
-    int L1=tedad_char(p1);//tedad character p1
-    int L2=tedad_char(p2);//tedad character p2
+    int L1=tedad_char(pName1);//tedad character pName1
+    int L2=tedad_char(pName2);//tedad character pName2
     int e=15;//space between arrays
-    for (int i = 0; i < ((2*n+1)/2)-L1/2; i++)//print space befor p1
+    for (int i = 0; i < ((2*n+1)/2)-L1/2; i++)//print space befor pName1
     {
         printf(" ");
     }
-    printf("%s",p1);
-    for (int i = 0; i <(((2*n+1)+e)+2*n+1)/2-(L2/2); i++)//print space between p1 and p2
+    printf("%s",pName1);
+    for (int i = 0; i <(((2*n+1)+e)+2*n+1)/2-(L2/2); i++)//print space between pName1 and pName2
     {
          printf(" ");
     }
-    printf("%d",p2);
+    printf("%d",pName2);
+    for (int i = 0; i <4*n+2 ; i++)
+    {
+         printf("-");
+    }
+    
 }
