d536cb2bfa16d9a8bea3fe92cfd8d6bf987f15b1
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2022-12-08 11:14:03 +0330

    update :wq

--- phase 1/main/main.c
+++ phase 1/main/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "welcome.h"
 
-int player1[16][16] = {};   //maximum battle area table is 15*15
-int player2[16][16] = {};   //it starts from [1][1]
+int player1[15][15] = {};   //maximum battle area table is 15*15
+int player2[15][15] = {};   //it starts from [1][1]
 
 int main()
 {
