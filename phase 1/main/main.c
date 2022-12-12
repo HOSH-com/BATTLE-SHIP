49f92e5223ec954a5d6661623c83a6fb14f2768f
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2022-12-12 18:53:12 +0330

    new input

--- phase 1/main/main.c
+++ phase 1/main/main.c
@@ -8,17 +8,20 @@ int player2[15][15] = {};
 
 int main()
 {
-    int nShip,x,y;
+    int areaSize, x, y, nShip;
     char pName1[21] = "FOCP1";
     char pName2[21] = "FOCP2";
     
+    scanf("%i", &areaSize);
     scanf("%i", &nShip);
     for(int i =0;i<3;i++)
     {
         scanf("%d%d",&x,&y);
         fire(x,y,player2);
     }
-    printTable(pName1 , pName2, player1, player2, nShip);
+
+
+    printTable(pName1 , pName2, player1, player2, areaSize);
    // printWelcome();
     
     return 0;
