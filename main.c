1db0fde0ef79fcf393a6da40a03409c05ef38824
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2022-12-17 20:07:43 +0330

    new input for names adde

--- main.c
+++ main.c
@@ -9,21 +9,15 @@ int player2[15][15] = {};
 int main()
 {
     int areaSize, x, y, nShip;
+    char pName1[21];
+    char pName2[21];    
+    
+    scanf("%i", &areaSize);     //1- size
+    scanf("%i", &nShip);        //2- ship amounts
+    scanf("%[^\n]s", pName1);   //3- player1 name
+    scanf("%[^\n]s", pName2);   //4- player2 name
 
-    char pName1[21] = "FOCP1";
-    char pName2[21] = "FOCP2";    
-    scanf("%i", &areaSize);
-    scanf("%i", &nShip);
-    for(int i =0;i<3;i++)//DAMAGE AREA PLAYER 1
-    {
-        scanf("%d%d",&x,&y);
-        fire(x,y,player1);
-    }
-    for(int i =0;i<3;i++)//DAMAGE AREA PLAYER 2
-    {
-        scanf("%d%d",&x,&y);
-        fire(x,y,player2);
-    }
     printTable(pName1 , pName2, player1, player2, areaSize);
+    
     return 0;
 }
