de93e07c8e11cba219cd936031b9aa1ba59b028a
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2022-12-17 22:00:59 +0330

    input string form changed

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "headers/welcome.h"
 #include "headers/table.h"
 #include "headers/ship.h"
@@ -9,13 +10,13 @@ int player2[15][15] = {};
 int main()
 {
     int areaSize, x, y, nShip;
-    char pName1[21];
-    char pName2[21];    
+    char pName1[21] = {};
+    char pName2[21] = {};    
     
     scanf("%i", &areaSize);     //1- size
     scanf("%i", &nShip);        //2- ship amounts
-    scanf("%[^\n]s", pName1);   //3- player1 name
-    scanf("%[^\n]s", pName2);   //4- player2 name
+    scanf("%s", pName1);        //3- player1 name
+    scanf("%s", pName2);        //4- player2 name
 
     printTable(pName1 , pName2, player1, player2, areaSize);
     
