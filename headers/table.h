827f3a0487b6d16d21466d496d7c58ad12dbbc18
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2022-12-24 13:14:35 +0330

    "row counting" mode changed

--- headers/table.h
+++ headers/table.h
@@ -38,17 +38,17 @@ void printNames(const char pName1[], const char pName2[], int n)
     int L1=tedad_char(pName1);//tedad character pName1
     int L2=tedad_char(pName2);//tedad character pName2
     int e=15;//space between arrays
-    for (int i = 0; i <(3+(3*n-L1)/2) - 1; i++)//print space befor pName1
+    for (int i = 0; i <(3+(3*n-L1)/2); i++)//print space befor pName1
     {
         printf(" ");
     }
     printf("%s",pName1);
-    for (int i = 0; i <3*(n+1)-(L1+L2)/2+14; i++)//print space between pName1 and pName2
+    for (int i = 0; i <3*(n+1)-(L1+L2)/2+15; i++)//print space between pName1 and pName2
     {
          printf(" ");
     }
     printf("%s\n",pName2);
-    for (int i = 0; i <6*(n+1)+13 ; i++)
+    for (int i = 0; i <6*(n+1)+14 ; i++)
     {
          printf("=");
     }
@@ -59,62 +59,19 @@ void printBattlefields(const int player1[][15], const int player2[][15], int n)
 {
     //first line:
         printf("   ");                                 //section 1: empty space 1
-        for (int i = 1; i <= 9 && i <= n; i++) {      //section 2: col nom 1
-            printf ("%i  ", i);
+        for (int i = 1; i <= n; i++) {      //section 2: col nom 1
+            printf ("%2i ", i);
         }
-        for (int i = 10; i <= n; i++) {      //...
-            printf ("%i ", i);
-        }
-        printf ("      |          ");                  //section 3: empty space 2
-        for (int i = 1; i <= 9 && i <= n; i++) {      //section 4: col nom 2
-            printf ("%i  ", i);
-        }
-        for (int i = 10; i <= n; i++) {      //...
-            printf ("%i ", i);
+        printf ("       |          ");                  //section 3: empty space 2
+        for (int i = 1; i <= n; i++) {      ////section 4: col nom 2
+            printf ("%2i ", i);
         }    
         printf("\n");                        //next line
     
     //other lines:
-        for (int i = 0; i < 9 && i < n; i++)              //main loop
+        for (int i = 0; i < n; i++)             //main loop with new condition
         {
-            printf("%i  ", i+1);               //section 1: row nom 1
-            for (int j = 0; j < n; j++)       //section 2: player 1
-            {
-                switch (player1[i][j]) 
-                {
-                    case NO_SHIP:
-                        printf("~  "); 
-                        break;
-                    case DESTROYED:
-                        printf("X  "); 
-                        break;
-                    default:
-                        if(player1[i][j] > 0) printf("■  ");
-                        break;
-                }
-            }
-            printf("      |       ");               //section 3: empty space
-            printf("%i  ", i+1);                    //section 4: row nom 1
-            for (int j = 0; j < n; j++)            //section 5: player 2
-            {   
-                switch (player2[i][j]) {
-                    case NO_SHIP:
-                        printf("~  "); 
-                        break;
-                    case DESTROYED:
-                        printf("X  "); 
-                        break;
-                    default:
-                        if(player2[i][j] > 0) printf("■  ");
-                        break;
-                }
-            }
-            printf("\n");       //next line
-        }
-        //resume:
-        for (int i = 9; i < n; i++)             //main loop with new condition
-        {
-            printf("%i ", i+1);               //section 1: row nom 1
+            printf("%2i  ", i+1);               //section 1: row nom 1
             for (int j = 0; j < n; j++)      //section 2: player 1
             {
                 switch (player1[i][j]) 
@@ -131,7 +88,7 @@ void printBattlefields(const int player1[][15], const int player2[][15], int n)
                 }
             }
             printf("      |       ");               //section 3: empty space
-            printf("%i ", i+1);                     //section 4: row nom 1
+            printf("%2i  ", i+1);                     //section 4: row nom 2
             for (int j = 0; j < n; j++)            //section 5: player 2
             {   
                 switch (player2[i][j]) {
