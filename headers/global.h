585014977fd8bc8313c8cdb5882c25659670babf
HESAM <hasam13831@gmail.com>
2023-01-05 16:34:30 +0330

    struct update

--- headers/global.h
+++ headers/global.h
@@ -1,7 +1,22 @@
 #ifndef global
 #define global
+#define MAX_COOR 20
 
 int player1_ship_Coordinates[20][4] = {0};
 int player2_ship_Coordinates[20][4] = {0};
+struct PLAYERS_INFO
+{
+    char name[21];
+    int number_of_ship;
+    int remaning_ship;
+    int battlefield[15][15];
+    int ship_coordinates[MAX_COOR][4];
+}player1[100],player2[100];
+struct GAME_SETTING
+{
+    int size_of_area;
+    int max_element;
+    int max_repair;
+}setting;
 
 #endif
