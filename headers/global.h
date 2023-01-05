599f82fc41f720cc0dca554ab9d82666ad098976
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2023-01-05 18:20:00 +0330

    struct mode applied to table.h

--- headers/global.h
+++ headers/global.h
@@ -1,13 +1,7 @@
-#ifndef global
-#define global
 #define MAX_COOR 20
 
-#define BLACK 0
-#define GREY 8
-#define WHITE1 7
-#define WHITE2 15
-#define BLUE 3
-#define RED 4
+#ifndef global
+#define global
 
 int player1_ship_Coordinates[20][4] = {0};
 int player2_ship_Coordinates[20][4] = {0};
@@ -15,7 +9,7 @@ struct PLAYERS_INFO
 {
     char name[21];
     int number_of_ship;
-    int remaning_ship;
+    int remaining_ship;
     int battlefield[15][15];
     int ship_coordinates[MAX_COOR][4];
 }player1[100],player2[100];
